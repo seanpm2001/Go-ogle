a8935997fa00887fda014d999361596d9793fab7
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-07 13:17:20 -0700

    INFO.go
    Info about this project.

---
+++ Go!-ogle/INFO.go
@@ -0,0 +1,3 @@
+// This project is written in Go! not Go (the 2009 Google programming language) Google originally claimed that there were "many" languages named Go at the time they created their language, and that it "wouldn't affect" the creator of Go! As of 2020, there still is only 2 languages named Go, the one by Keith Clark, and the one by Google.
+
+// This project is a degoogled version of Google search written in Go! By Degoogled, I mean it is more privacy focused, but gives Google search results. It will also serve another purpose eventually.
